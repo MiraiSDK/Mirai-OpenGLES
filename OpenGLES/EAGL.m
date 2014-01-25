78c01a5abf5d3a5fd41971558674af9b9716c3da
Yonghui Chen <chyhfj@gmail.com>
2014-01-25 17:44:02 +0800

    simple forward to EGL

--- OpenGLES/EAGL.m
+++ OpenGLES/EAGL.m
@@ -9,6 +9,10 @@
 #import "EAGL.h"
 #import <pthread.h>
 
+#include <EGL/egl.h>
+//#include <EGL/eglext.h>
+
+
 void EAGLGetVersion(unsigned int* major, unsigned int* minor)
 {
     *major = EAGL_MAJOR_VERSION;
@@ -23,7 +27,10 @@ void EAGLGetVersion(unsigned int* major, unsigned int* minor)
 }
 @end
 
-@implementation EAGLContext
+@implementation EAGLContext {
+    EGLContext *_eglContext;
+    EGLDisplay *_eglDisplay;
+}
 
 void pthread_key_destructor_callback(void *value)
 {
@@ -41,7 +48,20 @@ static pthread_key_t key = 0;
 {
     EAGLContext *prevContext = (__bridge_transfer EAGLContext *)(pthread_getspecific(key));
     int r = pthread_setspecific(key, (__bridge_retained const void *)(context));
-    return (r == 0);
+
+    if (r != 0) {
+        return NO;
+    }
+    
+    
+    EGLSurface surface = eglGetCurrentSurface(EGL_DRAW);
+    
+    BOOL success = eglMakeCurrent(context->_eglDisplay, surface, surface, context->_eglContext);
+    if (!success) {
+        NSLog(@"Unable to eglMakeCurrent");
+    }
+    
+    return success;
 }
 
 + (EAGLContext *)currentContext
@@ -60,6 +80,22 @@ static pthread_key_t key = 0;
     self = [super init];
     if (self) {
         _API = api;
+        
+        _eglDisplay = eglGetCurrentDisplay();
+        EGLConfig config;
+        EGLint numConfigs;
+        const EGLint attribs[] = {
+            EGL_SURFACE_TYPE, EGL_WINDOW_BIT,
+            EGL_BLUE_SIZE, 8,
+            EGL_GREEN_SIZE, 8,
+            EGL_RED_SIZE, 8,
+            EGL_NONE
+        };
+
+        eglChooseConfig(_eglDisplay, attribs, &config, 1, &numConfigs);
+
+        _eglContext = eglCreateContext(_eglDisplay, config, NULL, NULL);
+        
         _sharegroup = sharegroup;
         if (!_sharegroup) {
             _sharegroup = [[EAGLSharegroup alloc] initWithAPI:api];
@@ -67,6 +103,13 @@ static pthread_key_t key = 0;
         _debugLabel = sharegroup.debugLabel;
     }
     return self;
+}
+
+- (void)dealloc
+{
+    if (_eglContext != EGL_NO_CONTEXT) {
+        eglDestroyContext(_eglDisplay, _eglContext);
+    }
 }
 
 @end
