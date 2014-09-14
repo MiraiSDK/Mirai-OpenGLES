4589ac7cce5c520a225f146637b1c56a17b8e5d6
Yonghui Chen <chyhfj@gmail.com>
2014-09-14 15:39:12 +0800

    enable stencil buffer

--- OpenGLES/EAGL.m
+++ OpenGLES/EAGL.m
@@ -117,6 +117,7 @@ static EAGLSharegroup *gDefaultShareGroup = nil;
             EGL_BLUE_SIZE, 8,
             EGL_GREEN_SIZE, 8,
             EGL_RED_SIZE, 8,
+            EGL_STENCIL_SIZE, 8,
             EGL_NONE
         };
 
